fb9c8dbd6accc31ecd390d0fcf8a7e674e1e8c14
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-29 16:08:31 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,15 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     receiver.encoderStartStrecke(20)
     receiver.encoderSelectMotor(192)
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    btf.set_timeoutDisbled(true)
+    dauerhaft_Knopf_B = !(dauerhaft_Knopf_B)
+})
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonBhold()
 })
 btf.onReceivedData(function (receivedData) {
-    if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0Fahren) && btf.getaktiviert(receivedData, btf.e3aktiviert.m0)) {
+    if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0Fahren)) {
         receiver.sendM0(btf.btf_receivedBuffer19())
         receiver.writeQwiicRelay(btf.getSchalter(receivedData, btf.e0Schalter.b1))
     } else if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p2Fahrplan)) {
@@ -17,16 +21,38 @@ btf.onReceivedData(function (receivedData) {
     btf.zeige5x5Buffer(receivedData)
     btf.zeige5x5Joystick(receivedData)
     receiver.ringTone(btf.getSchalter(receivedData, btf.e0Schalter.b0))
+    lcd20x4.writeText(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4), 3, 0, 7, receiver.getQwiicUltrasonic(receiver.eI2CQwiicUltrasonic.x00))
 })
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonAhold()
 })
+let bWiederholung = false
+let dauerhaft_Knopf_B = false
 receiver.beimStart(
 receiver.eHardware.v3,
 96,
 true,
 67
 )
+lcd20x4.initLCD(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4))
+lcd20x4.writeText(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4), 0, 0, 19, lcd20x4.lcd20x4_text("Maker Kit Car"))
+basic.forever(function () {
+    if (dauerhaft_Knopf_B) {
+        receiver.beispielSpurfolger16(
+        192,
+        160,
+        31,
+        bWiederholung,
+        false,
+        20
+        )
+        bWiederholung = true
+    } else if (bWiederholung) {
+        dauerhaft_Knopf_B = false
+        bWiederholung = false
+        receiver.dualMotor128(receiver.eDualMotor.M0, 128)
+    }
+})
 loops.everyInterval(700, function () {
     if (btf.timeout(60000, true)) {
         receiver.pinRelay(false)
