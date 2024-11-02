71bd3d536f0826f6e4b3c127ebc0266b722671e3
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-11-02 13:23:25 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -145,8 +145,8 @@ receiver.onEncoderEvent(function (fahren, lenken, bp, ok, array) {
     }
     btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.b), receiver.encoderArray(array, receiver.eEncoderArray.colorb))
     btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.c), receiver.encoderArray(array, receiver.eEncoderArray.colorc))
-    btf.zeigeBIN(Math.idiv(receiver.encoderArray(array, receiver.eEncoderArray.iLinks), receiver.encoderArray(array, receiver.eEncoderArray.encoderFaktor)), btf.ePlot.map, 3)
-    btf.zeigeBIN(Math.idiv(receiver.encoderArray(array, receiver.eEncoderArray.iRechts), receiver.encoderArray(array, receiver.eEncoderArray.encoderFaktor)), btf.ePlot.map, 4)
+    btf.zeigeBIN(Math.idiv(Math.abs(receiver.encoderArray(array, receiver.eEncoderArray.iLinks)), receiver.encoderArray(array, receiver.eEncoderArray.encoderFaktor)), btf.ePlot.map, 3)
+    btf.zeigeBIN(Math.idiv(Math.abs(receiver.encoderArray(array, receiver.eEncoderArray.iRechts)), receiver.encoderArray(array, receiver.eEncoderArray.encoderFaktor)), btf.ePlot.map, 4)
 })
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonAhold()
